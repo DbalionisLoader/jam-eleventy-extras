c8290f0d835fc454639553df2107700d9e3758fd
Dovydas B <27741806+DbalionisLoader@users.noreply.github.com>
2023-07-24 19:29:46 +0100

    Fix logo path to show correctly on all pages

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,9 @@ module.exports = function(eleventyConfig) {
     // Return your Object options:
     eleventyConfig.addPassthroughCopy("./src/style.css");
     eleventyConfig.addPassthroughCopy("./src/assets");
+    eleventyConfig.addPassthroughCopy("src/assets/images");
     eleventyConfig.addPassthroughCopy("./src/js");
+
    
     return {
       dir: {
