4307137bf592e4210913368138c1b478be5873cd
Dovydas B <27741806+DbalionisLoader@users.noreply.github.com>
2023-10-19 19:40:29 +0100

    Add blog metadata, fix missing layout

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+module.exports = {
+	title: "Dovydas B. Code Adventures: Tips, Tricks and Tutorials",
+	url: "",
+	language: "en",
+	description: "Discover Practical Solutions to Real-Life Developer Challenges Solved – Dive into a treasure trove of coding insights and problem-solving wisdom on my dev blog",
+	author: {
+		name: "Dovydas Balionis",
+		email: "dbalionis@outlook.com",
+		url: "https://dovydasbalionis.co.uk/"
+	}
+}
